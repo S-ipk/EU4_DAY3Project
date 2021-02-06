28eea9a77bf3bd8ebc1183cf3371f91275966fd7
Safiye Ipek <safiye.ipek96@gmail.com>
2021-02-06 15:26:39 +0000

    changed a message

--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -2,7 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello Git Last Day  ");
-        //eu4 is extited
+        //eu4 is excited
         //eu4 is dedicated!
         //ue4 is successful testers
     }
